1cf3dc4f62e03e5f2fea182fff31f9fc53a9751f
Rafael Lima <rafael_weblima@hotmail.com>
2020-12-22 15:41:22 -0300

    Criação das classes e implementação no programa princial

---
+++ Conjuntos/Entities/Students.cs
@@ -0,0 +1,19 @@
+﻿namespace Conjuntos.Entities {
+    class Students {
+        public string Course { get; set; }
+        public int Usuario { get; set; }
+
+        public override int GetHashCode() {
+            return Usuario.GetHashCode();
+        }
+
+        public override bool Equals(object obj) {
+            if (!(obj is Students)) {
+                return false;
+            }
+
+            Students other = obj as Students;
+            return Usuario.Equals(other.Usuario);
+        }
+    }
+}
---
+++ Conjuntos/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using Conjuntos.Entities;
+
+namespace Conjuntos {
+    class Program {
+        static void Main(string[] args) {
+
+            HashSet<Students> set = new HashSet<Students>();
+
+            string[] courses = new string[3] { "A", "B", "C" };
+
+            foreach (string course in courses) {
+                Console.Write($"How many students for course {course}? ");
+                int n = int.Parse(Console.ReadLine());
+                for (int i = 1; i <= n; i++) {
+                    
+                    int usuario = int.Parse(Console.ReadLine());
+
+                    set.Add(new Students {Course=course,Usuario=usuario});
+                }
+            }
+            Console.WriteLine("Total students: " + set.Count);
+        }
+    }
+}
